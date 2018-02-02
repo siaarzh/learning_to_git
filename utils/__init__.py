ad6babb8140e6624bf08a291043751d1f76d1e2b
siaarzh <siaarzh@gmail.com>
2018-02-02 16:25:35 +0600

    Initial local commit

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from utils.multiplier import Multiplier
---
+++ utils/multiplier.py
@@ -0,0 +1,11 @@
+class Multiplier(object):
+
+	def __init__(self):
+		pass
+
+	def mult(self, *args):
+
+		for i in args:
+			product = 1
+			product = product*i
+		return product
