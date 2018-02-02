484ea1c6fba3e2654fa7a948d94318a2bd26bbf9
siaarzh <siaarzh@gmail.com>
2018-02-02 16:26:59 +0600

    Added Adder class

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from utils.multiplier import Multiplier
+from utils.adder import Adder
---
+++ utils/adder.py
@@ -0,0 +1,11 @@
+class Adder(object):
+
+	def __init__(self):
+		pass
+
+	def add(self, *args):
+
+		for i in args:
+			product = 1
+			product = product+i
+		return product
