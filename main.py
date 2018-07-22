af51836a5b54c1130549da9dcd27e7d1dfaf8f92
Serzhan (win7) <siaarzh@gmail.com>
2018-07-22 16:15:46 +0600

    added new stuff to master

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ numbers = [1, 2, 3, 4]
 print("Sum = {}".format(A.add(*numbers)))
 # line for checking SSH connection
 print("Product = {}".format(P.mult(*numbers)))
+
+# some new stuff here:
+print("Hello, it me your looking for?")
